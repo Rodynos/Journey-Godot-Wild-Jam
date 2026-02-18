049039034d3cf3b8f5f5b7efe91ce5d1893d3609
joe2 <533331@student.fontys.nl>
2026-02-18 15:08:57 +0100

    Safety Commit

--- Player/player.gd
+++ Player/player.gd
@@ -4,7 +4,7 @@ const SPEED = 80
 
 var current_direction
 
-enum{
+enum direction{
 	UP,
 	UP_RIGHT,
 	UP_LEFT,
@@ -23,15 +23,65 @@ var KEY_LEFT = false
 
 func _process(delta: float) -> void:
 	pass
+	set_input()
+	set_direction()
+	move()
 
-func input_in_action():
+func set_input():
 	read_input(KEY_UP, "move_up")
 	read_input(KEY_DOWN, "move_down")
 	read_input(KEY_RIGHT, "move_right")
 	read_input(KEY_LEFT, "move_left")
 
+
+func set_direction():
+	if KEY_UP:
+		if KEY_LEFT:
+			current_direction = direction.UP_LEFT
+		elif KEY_RIGHT: 
+			current_direction = direction.UP_RIGHT
+		else: current_direction = direction.UP
+	elif KEY_DOWN:
+		if KEY_LEFT:
+			current_direction = direction.DOWN_LEFT
+		elif KEY_RIGHT: 
+			current_direction = direction.DOWN_RIGHT
+		else: current_direction = direction.DOWN
+	elif KEY_LEFT:
+		current_direction = direction.LEFT
+	elif KEY_RIGHT:
+		current_direction = direction.RIGHT
+	else: current_direction - direction.IDLE
+
+
+func move():
+	match current_direction:
+		direction.UP:
+			self.velocity = Vector2(0, -SPEED)
+			# $AnimatedSprite2D.play("up")
+		direction.UP_RIGHT: pass
+		direction.UP_LEFT: pass
+		direction.DOWN:
+			self.velocity = Vector2(0, SPEED)
+			# $AnimatedSprite2D.play("down")
+		direction.DOWN_RIGHT: pass
+		direction.DOWN_LEFT: pass
+		direction.LEFT:
+			self.velocity = Vector2(-SPEED, 0)
+			# $AnimatedSprite2D.play("left")
+		direction.RIGHT:
+			self.velocity = Vector2(SPEED, 0)
+			# $AnimatedSprite2D.play("right")
+		direction.IDLE:
+			self.velocity = Vector2(0, 0)
+			# $AnimatedSprite2D.play("idle")
+		
+	move_and_slide()
+
+
 func cartesian_to_isometric(cartesian):
 	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) / 2)
+
 
 func read_input(KEY: bool, action: StringName):
 	if Input.is_action_pressed(action): return true
